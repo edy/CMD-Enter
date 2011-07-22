0fdc0324e62ade22efb450d3809015deed80a0b7
Edy <eduard@baun.de>
2011-07-22 20:00:11 +0200

    added subdirectory

--- create_xpi.sh
+++ create_xpi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-zip cmdenter-2.4-fx-mac.xpi ./install.rdf ./chrome.manifest ./icon.png ./chrome/*
+zip cmdenter-2.4-fx-mac.xpi ./install.rdf ./chrome.manifest ./icon.png ./chrome/content/*
