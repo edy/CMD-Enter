8c01720d5c4334d39cc9c671d34628e7f51c9592
Edy <eduard@baun.de>
2011-07-22 19:58:39 +0200

    forgot to update the build script

--- create_xpi.sh
+++ create_xpi.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-zip cmdenter-2.3-fx-mac.xpi ./install.rdf ./chrome.manifest ./icon.png ./overlays/*
+zip cmdenter-2.4-fx-mac.xpi ./install.rdf ./chrome.manifest ./icon.png ./chrome/*
